dc987c56acfbddc3db424fbbf134ed9000f02311
Kyronne <kyronne.parkes@gmail.com>
2022-12-20 23:53:13 +0000

    Added OpenAI prompt to script

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -1,4 +1,13 @@
-const generateCompletionAction = async (info) => {}
+const generateCompletionAction = async (info) => { try {
+    const { selectionText } = info;
+    const basePromptPrefix = `
+	I want you to act as a wellbeing coach, giving advice in a supportive way help the person to be resilient by making this advice achievable and actionable. Take inspiration from Gabor Maté, Marsha P Linehan and Brené Brown in your response. 
+    Advice:
+	`;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({

f22f2f9cdc0e5cd1cdd1b531bf6e43a97473abd4
Kyronne <kyronne.parkes@gmail.com>
2022-12-24 22:46:12 +0000

    Edited prompt and changed code formatting

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -1,7 +1,7 @@
-const generateCompletionAction = async (info) => { try {
+const generateCompletionAction = async (info) => { 
+    try {
     const { selectionText } = info;
-    const basePromptPrefix = `
-	I want you to act as a wellbeing coach, giving advice in a supportive way help the person to be resilient by making this advice achievable and actionable. Take inspiration from Gabor Maté, Marsha P Linehan and Brené Brown in your response. 
+    const basePromptPrefix = `I want you to act as a wellbeing coach, giving advice in a supportive conversational way help the person to be resilient by making this advice achievable and actionable. Take inspiration from Gabor Maté, Marsha P Linehan and Brené Brown in your response where helpful. 
     Advice:
 	`;
   } catch (error) {
